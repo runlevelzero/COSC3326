b54bff3af20a16426c34496d178d541728280a96
Ruben <runlevelzero@users.noreply.github.com>
2020-01-21 13:29:15 -0600

    Project 1 - I am Rich

---
+++ Project 1 - I am Rich/Project 1 - I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 1 - I am Rich
+//
+//  Created by Turing on 1/21/20.
+//  Copyright © 2020 Ruben Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
