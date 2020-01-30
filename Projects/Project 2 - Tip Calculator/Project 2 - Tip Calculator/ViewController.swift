3b6311f88c730fd44db7a41354412aad473a9d6a
Ruben Ruiz <ruben.s.ruiz@outlook.com>
2020-01-30 09:36:26 -0600

    What we worked on last class

--- Projects/Project 2 - Tip Calculator/Project 2 - Tip Calculator/ViewController.swift
+++ Projects/Project 2 - Tip Calculator/Project 2 - Tip Calculator/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /* Var Bloc //////////////////////////////////////////////// */
+    @IBOutlet weak var tipPickerControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    /*////////////////////////////////////////////////////////// */
+    
+    /*////////////////////////////////////////////////////////// */
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    /* Code Bloc /////////////////////////////////////////////// */
+    @IBAction func calculateTipBillAmount(_ sender: UIButton) {
+        
+        let billAmount          = Float(billAmountTextField.text!)
+        let gratuityIndex: Int  = tipPickerControl.selectedSegmentIndex
+        let gratuity: Float     = (gratuityIndex == 0) ? 0.10 : (gratuityIndex == 1) ? 0.15 : 0.75
+        let tipAmount           = billAmount! * gratuity
+        let totalBill           = billAmount! + tipAmount
+        let billDisplay         = String(format: "$%.2f", totalBill)
+        let tipDisplay          = String(format: "$%.2f", tipAmount)
 
+        
+        
+        billAmountLabel.text    = billDisplay
+        tipAmountLabel.text     = tipDisplay
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    /*////////////////////////////////////////////////////////// */
 
 }
 
--- Swift Cheatsheet.md
+++ Swift Cheatsheet.md
@@ -5,6 +5,31 @@
         alertController.addAction(UIAlertAction(title: "Action Title Here", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
+
+
 ```
 
+## Outlets vs Actions
+### Action
+Works like a function and runs the code when called
+### Outlet
+Essentially a variable that represents the element
+
+## Sample Variable Definition
+```js
+	var varName: varType
+``` 
+
+## Optionals
+We'll Get back to this soon
+
+## How to click out of the Keyboard
+When the keyboard is out, you are editing data
+You need to tell the app that you are done editing
+Here is the code:
+```js
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        	self.view.endEditing(true)
+    	}
+```
 
